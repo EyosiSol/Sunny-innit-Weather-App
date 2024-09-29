84cf82e4ba5d77a092e42a630f0fcae18682e826
Eyosi Sol <eyosila123@gmail.com>
2024-09-29 15:55:15 +0300

    work on the header's css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import weather from './weather';
 import './styles/main.css';
+import logo from './assets/Logo.png'
+import search from './assets/Search.png'
+
+const logodiv = document.querySelector('.logo')
+const img = document.createElement('img')
+img.src = logo;
+logodiv.appendChild(img)
+
+const searchbar = document.querySelector('.search')
+const searchIcon = document.createElement('img')
+searchIcon.src = search;
+searchIcon.classList.add('searchIcon')
+searchbar.appendChild(searchIcon);
+
+const body = document.body
+
 console.log('working');
 weather();
